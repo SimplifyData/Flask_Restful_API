96089abaa7f15040fddcdfa58a55861c7730c4b4
az <atif.zafar@sds.technology>
2019-12-11 01:59:17 -0500

    Final Commit of Flask Restful API Project

---
+++ resources/item.py
@@ -0,0 +1,76 @@
+import sqlite3
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
+
+from models.item import ItemModel
+
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+                        type=float,
+                        required=True,
+                        help="This field cannnot be left blank")
+    parser.add_argument('store_id',
+                        type=int,
+                        required=True,
+                        help="Every item needs a store id")
+
+    @jwt_required()
+    def get(self, name):
+        try:
+            item = ItemModel.find_by_name(name)
+        except:
+            {"message": "An error occurred inserting the item"}
+
+        if item:
+            return item.json()
+        return {"message": "Item not found"}, 404
+
+    def post(self, name):
+        if ItemModel.find_by_name(name):
+            return {'message': "An item with name '{}' already exists.".format(name)}, 400  # 400: bad request
+
+        data = Item.parser.parse_args()
+
+        item = ItemModel(name, **data)
+        try:
+           item.save_to_db()
+        except:
+            return {"message": "An error occurred inserting the item"}, 500
+
+        return item.json(), 201  # http code for 'creating' = 201
+
+    def delete(self,name):
+        item = ItemModel.find_by_name(name)
+
+        if item:
+           item.delete_from_db()
+
+        return {"message": "Item Deleted"}
+
+
+
+    def put(self,name):
+        data = Item.parser.parse_args()
+
+        item = ItemModel.find_by_name(name)
+
+
+        if item is None:
+            try:
+                item = ItemModel(name, **data)
+            except:
+                return {"message": "An error occurred inserting the item"}, 500
+        else:
+            try:
+                item.price = data['price']
+            except:
+                return {"message": "An error occurred updating the item"}, 500
+        item.save_to_db()
+
+        return item.json()
+
+
+class ItemList(Resource):
+    def get(self):
+        return {"items": list(map(lambda x: x.json(), ItemModel.query.all()))}
---
+++ resources/store.py
@@ -0,0 +1,42 @@
+from flask_restful import Resource, reqparse
+from flask_jwt import jwt_required
+
+from models.store import StoreModel
+
+class Store(Resource):
+
+
+    def get(self, name):
+        try:
+            store = StoreModel.find_by_name(name)
+        except:
+            {"message": "An error occurred inserting the item"}
+
+        if store:
+            return store.json()
+        return {"message": "Store not found"}, 404
+
+    def post(self, name):
+        if StoreModel.find_by_name(name):
+            return {'message': "An Store with name '{}' already exists.".format(name)}, 400  # 400: bad request
+
+        store = StoreModel(name)
+        try:
+           store.save_to_db()
+        except:
+            return {"message": "An error occurred inserting the item"}, 500
+
+        return store.json(), 201  # http code for 'creating' = 201
+
+    def delete(self,name):
+        store = StoreModel.find_by_name(name)
+
+        if store:
+           store.delete_from_db()
+
+        return {"message": "Store Deleted"}
+
+
+class StoreList(Resource):
+    def get(self):
+        return {"stores": list(map(lambda x: x.json(), StoreModel.query.all()))}

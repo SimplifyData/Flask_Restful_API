19b20bf48adcd443f7b5a93319012635a5385c36
az <atif.zafar@sds.technology>
2019-12-11 05:15:20 -0500

    PostGres For Heroku

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_jwt import JWT
 
 from security import authenticate, identity
 
-from resources.user import UserRegister
+from resources.user import UserRegister, User
 
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
@@ -17,6 +17,7 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI']= os.environ.get('DATABASE_URL','sqlite:///data.db')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'yoman'
 db.init_app(app)
 api = Api(app)
@@ -32,6 +33,7 @@ api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
+api.add_resource(User, '/user/<int:user_id>')
 
 if __name__ == "__main__":
 

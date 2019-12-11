46a68efc74354e057a512ca33616499738a93ab4
az <atif.zafar@sds.technology>
2019-12-11 08:38:19 -0500

    blacklist added

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
+from blacklist import BLACKLIST
 
 from security import authenticate, identity
 
@@ -18,6 +19,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']= os.environ.get('DATABASE_URL','sqlite:///data.db')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
 app.secret_key = 'yoman'
 db.init_app(app)
 api = Api(app)
@@ -33,6 +36,10 @@ def add_claims_to_jwt(identity):
     if identity == 1:
         return {'is_admin': True}
     return {'is_admin': False}
+
+@jwt.token_in_blacklist_loader
+def check_if_token_in_backlist(decrypted_token):
+    return decrypted_token['identity'] in BLACKLIST
 
 @jwt.expired_token_loader
 def expired_token_callback():

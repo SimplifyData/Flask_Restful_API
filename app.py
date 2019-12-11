45a416b7b5452733bcff56f49b23c0e0263a5d30
az <atif.zafar@sds.technology>
2019-12-11 02:50:46 -0500

    PostGres For Heroku

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
@@ -14,7 +15,7 @@ from db import db
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI']= os.environ.get('DATABASE_URL','sqlite:///data.db')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = 'yoman'
 db.init_app(app)
